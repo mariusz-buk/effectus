b09a2c25214c8a6437fbb11d9b180d885bbfd9f6
Gury8 <64693520+Gury8@users.noreply.github.com>
2020-05-23 23:21:04 +0200

    Effectus - new version 0.4.3

---
+++ examples/rainbow.pas
@@ -0,0 +1,38 @@
+// Effectus auto-generated Mad Pascal source code listing
+program rainbowPrg;
+
+uses
+  SySutils, Crt, Graph;
+
+// Effectus example
+// - - - - - - - - - - - - - - - - - -
+// Rainbow graphics demo
+// **********************************************************************
+// *                                                                    *
+// * PhoeniX SoftCrew  ACTION! Programme                                *
+// **********************************************************************
+// Programmname    :Rainbow
+// Filename        :RAINBOW.ACT
+// von             :Carsten Strotmann
+// letzte Aenderung:12.10.91
+// Bemerkung       :fuer M.Heinzig
+
+procedure RAINBOWProc;
+var
+  vcount : byte absolute $D40B;
+  wsync : byte absolute $D40A;
+  rtclok : byte absolute $14;
+  getcolor : byte absolute $D018;
+  u : byte absolute $7;
+  ch : byte absolute 764;
+begin  // 2
+  ch := $FF;
+  repeat
+  wsync := u;
+  getcolor := vcount+rtclok;
+  until  ch<>$FF;
+end;  // 4
+
+begin
+  RAINBOWProc;
+end.
