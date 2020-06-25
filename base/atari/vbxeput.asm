5a5e0de888da72b03afa0bf6549223509ba523a4
Gury8 <64693520+Gury8@users.noreply.github.com>
2020-06-25 18:02:44 +0200

    Add files via upload

---
+++ base/atari/vbxeput.asm
@@ -0,0 +1,45 @@
+
+; VBXE put char
+; input:
+;	A - char
+
+.proc	@vbxe_put (.byte a) .reg
+
+	pha
+
+	cmp #eol
+	beq stop
+
+	cmp #$7d		; clrscr
+	bne skp
+
+	jsr @vbxe_init.cmapini
+	jmp stop
+
+skp	lda rowcrs
+	pha
+	:4 lsr @
+	add #$80+MAIN.SYSTEM.VBXE_MAPADR/$1000
+	fxsa FX_MEMS
+
+	pla
+	and #$0f
+	add >MAIN.SYSTEM.VBXE_WINDOW
+	sta bp+1
+
+	lda colcrs
+	asl @
+	asl @
+	tay
+	mva colpf0s (bp),y
+	iny
+	mva colpf1s (bp),y
+	iny
+	mva colpf2s (bp),y
+
+	fxs FX_MEMS #$00
+
+stop	pla
+
+	rts
+.endp
