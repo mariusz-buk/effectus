a2dd730bf7b098695606e98b58df57581b73ff5d
Gury8 <64693520+Gury8@users.noreply.github.com>
2020-07-09 19:48:02 +0200

    New version 0.5.1
    New features
    - INCLUDE directive supported:
      - files can be included, starting in the same directory as main compiled listing code
      - currently one level of included files is supported. This means only main listing program can read other files with the use of INCLUDE directive (this directive will be ignored in included files)
      - file is ignored if it isn't found
    
      Example:
        INCLUDE "D1:LIBRARY.EFF"
        INCLUDE "H1:lib_test.eff"
        INCLUDE "TEST01.EFF"
    
    - Shell parameter -nc flag changed to -t (Effectus only translate source to Mad Pascal) - changed by Zbyti
    - IF condition operators AND and OR supported (every condition must be surrounded by () pair for the program to be compiled properly)
    
      Example:
        IF (SIZEP(3) = 1) AND (P0 = P0HPOS) AND (A = 2) THEN
        IF (T1 = 1) OR (T2 = 3) OR (isFlag = 0) THEN
    
    Bug fixes
      - Proper parsing of FUNCtions inside IF condition block
      - Proper handling of zero-page addresses from $A0 to $AF as parameters for machine language routines inside PROCedures and FUNCtions
      - Code refactoring and bug fixes by Zbyti:
         - doubled operator fix with all operands
         - SHL -> LSH bug correction
         - LSH -> SHR bug correction

---
+++ examples/include.pas
@@ -0,0 +1,51 @@
+// Effectus auto-generated Mad Pascal source code listing
+program includePrg;
+
+uses
+  Crt, Graph;
+
+// Effectus example
+// INCLUDE demo
+// Effectus example
+// User library for INCLUDE demo
+var
+  CH : byte absolute 764;
+
+procedure WAITFORKEYProc;
+begin  // 1
+  CH := 255;
+ WHILE CH=255 do begin
+  end;  // while
+end;  // 4
+
+procedure PRINTTEXTProc;
+begin  // 6
+  Writeln('Hello world');
+end;  // 4
+
+procedure CHANGECOLORProc;
+begin  // 6
+  Poke(710, 50);
+  Poke(712, 160);
+end;  // 4
+
+procedure MAINProc;
+begin  // 6
+  Write(Chr(125));
+  Writeln('Effectus example:');
+  Writeln('INCLUDE demonstration');
+  Writeln('');
+  Writeln('Press any key and see my greetings!');
+  WAITFORKEYProc;
+  Writeln('');
+  PRINTTEXTProc;
+  Writeln('');
+  Writeln('Press any key to change color!');
+  WAITFORKEYProc;
+  CHANGECOLORProc;
+  WAITFORKEYProc;
+end;  // 4
+
+begin
+  MAINProc;
+end.
